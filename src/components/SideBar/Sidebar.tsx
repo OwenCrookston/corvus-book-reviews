3ac1d15c7cc1e6575e57db489eb6d26a51f9778c
Owen Crookston <owen.crookston@gmail.com>
2025-01-31 13:35:10 -0800

    bunch of css changes, background color, responsiveness, single column on small screens, etc

--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -10,23 +10,33 @@ function Sidebar({ open, handleSidebarChange }: SidebarProps) {
     const dynamicClasses: string[] = [];
     open ? dynamicClasses.push("open") : dynamicClasses.push("closed");
 
-    return open ? (
-        <div className={`sidebar ${dynamicClasses.join(" ")}`}>
-            <div className="search-icon-row">
+    return (
+        <div className="sidebar-container">
+            {open ? (
+                <div className={`sidebar ${dynamicClasses.join(" ")}`}>
+                    <div className="search-icon-row">
+                        <SearchButton
+                            handleSidebarChange={() => handleSidebarChange()}
+                        />
+                        Search
+                    </div>
+                    <input
+                        type="search"
+                        placeholder="Title / Author"
+                        maxLength={30}
+                    />
+                    <select>
+                        <option value="">--Year Read--</option>
+                        <option value="2025">2025</option>
+                    </select>
+                    <div>Top Lists</div>
+                </div>
+            ) : (
                 <SearchButton
                     handleSidebarChange={() => handleSidebarChange()}
                 />
-                Search
-            </div>
-            <input type="search" placeholder="Title / Author" maxLength={30} />
-            <select>
-                <option value="">--Year Read--</option>
-                <option value="2025">2025</option>
-            </select>
-            <div>Top Lists</div>
+            )}
         </div>
-    ) : (
-        <SearchButton handleSidebarChange={() => handleSidebarChange()} />
     );
 }
 
